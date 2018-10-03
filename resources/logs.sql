7d29920d56670112aadba2317161b3accda17460
Oliver Marshall <oliver-marshall@hotmail.com>
2018-10-03 23:26:46 +0100

    Reformat logs.sql

--- resources/logs.sql
+++ resources/logs.sql
@@ -1,8 +1,7 @@
 -- src/obelisk_log/fanspeed.sql
 -- Obelisk logs
 
--- :name add-log :!
+-- :name add-log :! :n
 -- :doc Add a new log for a given time
-insert
-into `obelisk-logs` (`date`, `fan0`, `fan1`)
+insert into `obelisk-logs` (`date`, `fan0`, `fan1`)
 values (:date, :fan0, :fan1)
